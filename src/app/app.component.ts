77236f59e04ee2ab894aea50731330e82d841ed7
MSK998 <mwscottkiddie@gmail.com>
2020-10-30 20:57:10 +0000

    basic frontend for msk-url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+const BACKEND_URL = environment.apiURL + '/api/short';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'msk-url-frontend';
+  title = 'MSK-URL';
+  public shortURL: string;
+
+  constructor(private http: HttpClient) {}
+
+  submitURL(link: string): void {
+    const longURL = {
+      url: link,
+    };
+
+    this.http
+      .post<{ shortURL: string }>(BACKEND_URL, longURL)
+      .subscribe((response) => {
+        this.shortURL = response.shortURL;
+
+        console.log(this.shortURL);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input'
+import { MatButtonModule } from '@angular/material/button'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
